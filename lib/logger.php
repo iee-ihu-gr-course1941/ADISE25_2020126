c9f5efffdf1fda0c5c7ecd5f19472bd8443a9264
spyrospap10 <spyrospapadopoulos186@gmail.com>
2026-01-12 16:39:35 +0200

    .htaccess settings

--- lib/logger.php
+++ lib/logger.php
@@ -1,11 +1,12 @@
 <?php
-
 function app_log(string $message, string $level = 'INFO'): void
 {
     $logFile = __DIR__ . '/../logs/app.log';
-
     $date = date('Y-m-d H:i:s');
+    
+    // Προσθήκη επιπέδου και ημερομηνίας
     $line = "[$date] [$level] $message" . PHP_EOL;
 
+    // Εγγραφή (το FILE_APPEND προσθέτει στο τέλος)
     file_put_contents($logFile, $line, FILE_APPEND | LOCK_EX);
 }
