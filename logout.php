c58cae6dca58d7e03936baf6a18c9fbc341b2b4d
spyrospap10 <spyrospapadopoulos186@gmail.com>
2026-01-30 22:04:24 +0200

    Update logout.php

--- logout.php
+++ logout.php
@@ -5,27 +5,23 @@ session_start();
 if (isset($_SESSION['game_mode']) && $_SESSION['game_mode'] == 'online') {
     if(isset($_SESSION['token'])) {
         $token = $_SESSION['token'];
-        $sql = "SELECT piece_color FROM players WHERE token = '$token'";
-        $res = $mysqli->query($sql);
-        
+        $st = $mysqli->prepare("SELECT piece_color FROM players WHERE token = ?");
+        $st->bind_param("s", $token);
+        $st->execute();
+        $res = $st->get_result();
         if ($row = $res->fetch_assoc()) {
             $my_color = $row['piece_color'];
             $winner = ($my_color == 'W') ? 'B' : 'W';
-            
-            // 1. Στέλνουμε σήμα aborted. 
-            // ΠΡΟΣΟΧΗ: Δεν καλούμε clean_board ακόμα για να προλάβει να το δει ο άλλος!
-            $mysqli->query("UPDATE game_status SET status='aborted', result='$winner' WHERE status='started' OR status='first_roll'");
+            $mysqli->query("UPDATE game_status SET status='aborted', result='$winner' WHERE status IN ('started', 'first_roll', 'ended')");
         }
     }
 }
 
-// 2. Καθαρίζουμε ΤΟΥΣ ΠΑΝΤΕΣ από τον πίνακα players
+$mysqli->query("call clean_board()"); 
 $mysqli->query("UPDATE players SET username=NULL, token=NULL, last_action=NULL");
 
-// 3. Μηδενίζουμε τα σκορ
-$mysqli->query("UPDATE game_status SET score_w=0, score_b=0");
+$mysqli->query("UPDATE game_status SET status='not active', p_turn=NULL, result=NULL, score_w=0, score_b=0, last_change=NOW()");
 
-// 4. Καταστροφή Session
 session_unset();
 session_destroy();
 
